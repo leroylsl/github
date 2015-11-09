1a04f1626694b0626e5f2f8610e8dbdf80f56eae
xieShengCheng <708954644@qq.com>
2015-11-09 10:07:14 +0800

    搭建框架

---
+++ github/github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github
+//
+//  Created by 谢胜程 on 15/11/9.
+//  Copyright © 2015年 xsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ github/github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  github
+//
+//  Created by 谢胜程 on 15/11/9.
+//  Copyright © 2015年 xsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
